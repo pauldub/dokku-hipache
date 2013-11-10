17cd7f1aae02eb064e7f3a6f9dae93aca536793e
Paul d'Hubert <paul.dhubert@yandex.ru>
2013-11-10 12:28:29 +0100

    expand -t2

--- hipache.sh
+++ hipache.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 if [[ -f $DOKKU_ROOT/HIPACHE_REDIS ]]; then
-	HIPACHE_REDIS_URL=$(cat $DOKKU_ROOT/HIPACHE_REDIS)
+  HIPACHE_REDIS_URL=$(cat $DOKKU_ROOT/HIPACHE_REDIS)
 else
-	HIPACHE_REDIS_URL=127.0.0.1
+  HIPACHE_REDIS_URL=127.0.0.1
 fi
 
 if [[ -f $DOKKU_ROOT/HIPACHE_REDIS_PORT ]]; then
-	HIPACHE_REDIS_PORT=$(cat $DOKKU_ROOT/HIPACHE_REDIS_PORT)
+  HIPACHE_REDIS_PORT=$(cat $DOKKU_ROOT/HIPACHE_REDIS_PORT)
 else
-	HIPACHE_REDIS_PORT=6379
+  HIPACHE_REDIS_PORT=6379
 fi
 
 function redis {
-	redis-cli -h $HIPACHE_REDIS_URL -p $HIPACHE_REDIS_PORT $*
+  redis-cli -h $HIPACHE_REDIS_URL -p $HIPACHE_REDIS_PORT $*
 }
 
 function hipache_delete {
-	redis del frontend:$1 >/dev/null
+  redis del frontend:$1 >/dev/null
 }
 
 function hipache_frontend {
-	redis rpush frontend:$1 $2 >/dev/null
+  redis rpush frontend:$1 $2 >/dev/null
 }
 
 function hipache_backend {
-	redis rpush frontend:$1 $2 >/dev/null
+  redis rpush frontend:$1 $2 >/dev/null
 }
 
 function hipache_info {
-	redis lrange frontend:$1 0 -1
+  redis lrange frontend:$1 0 -1
 }
 
 function hipache_configure {
-	echo $1 > $DOKKU_ROOT/HIPACHE_REDIS
+  echo $1 > $DOKKU_ROOT/HIPACHE_REDIS
 }
 
 function app_hostname {
-		PORT=$(< "$DOKKU_ROOT/$1/PORT")	
-		VHOST=$(< "$DOKKU_ROOT/VHOST")
-		SUBDOMAIN=${1/%\.${VHOST}/}
-		if [[ "$1" == *.* ]] && [[ "$SUBDOMAIN" == "$APP" ]]; then
-			hostname="${1/\//-}"
-		else
-			hostname="${1/\//-}.$VHOST"
-  	fi
+    PORT=$(< "$DOKKU_ROOT/$1/PORT") 
+    VHOST=$(< "$DOKKU_ROOT/VHOST")
+    SUBDOMAIN=${1/%\.${VHOST}/}
+    if [[ "$1" == *.* ]] && [[ "$SUBDOMAIN" == "$APP" ]]; then
+      hostname="${1/\//-}"
+    else
+      hostname="${1/\//-}.$VHOST"
+    fi
 }
 
